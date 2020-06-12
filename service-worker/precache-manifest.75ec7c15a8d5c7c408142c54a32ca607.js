fad43a8a5d8cd6d5d20fc3a21a277a65179a4703
Mister-Hope <zhangbowang1998@gmail.com>
2020-06-12 12:02:07 +0000

    Deploying to gh-pages from  @ 18991a0e893a11ab0ed301a95d773dbfb4dc6e0b 🚀

---
+++ service-worker/precache-manifest.75ec7c15a8d5c7c408142c54a32ca607.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b4d9818a37177918dd",
+    "url": "/css/app.ca26ab96.css"
+  },
+  {
+    "revision": "d2146b414258529bf043",
+    "url": "/css/chunk-57a3ce61.03f7ef6f.css"
+  },
+  {
+    "revision": "962154a08feadf19e848ec6325021c98",
+    "url": "/img/logo.962154a0.png"
+  },
+  {
+    "revision": "4a798b9e6c1ba853714787f95c4c10ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b4d9818a37177918dd",
+    "url": "/js/app-legacy.165f30ca.js"
+  },
+  {
+    "revision": "d2146b414258529bf043",
+    "url": "/js/chunk-57a3ce61-legacy.109e255e.js"
+  },
+  {
+    "revision": "847c3387a839a27b93c3",
+    "url": "/js/manifest-legacy.908f78d5.js"
+  },
+  {
+    "revision": "d21ed065f47d1cef6bc8e1428844b128",
+    "url": "/manifest.json"
+  }
+]);
---
+++ service-worker/precache-manifest.f3752b3b84729397769563d700762b3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f68a9ab458cfcc9562ef",
+    "url": "/css/app.ca26ab96.css"
+  },
+  {
+    "revision": "a4f098e5dfee7058cafb",
+    "url": "/css/chunk-60df32a6.03f7ef6f.css"
+  },
+  {
+    "revision": "962154a08feadf19e848ec6325021c98",
+    "url": "/img/logo.962154a0.png"
+  },
+  {
+    "revision": "0814d6a74dcd1a004b5d3bd74602680a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f68a9ab458cfcc9562ef",
+    "url": "/js/app.5b4ce6f2.js"
+  },
+  {
+    "revision": "a4f098e5dfee7058cafb",
+    "url": "/js/chunk-60df32a6.adcf99cf.js"
+  },
+  {
+    "revision": "68271711a7b31c9613fd",
+    "url": "/js/manifest.29de4b87.js"
+  },
+  {
+    "revision": "6594b1c0496c2436bd1f1f5c88ee8b27",
+    "url": "/lightshow.mp4"
+  },
+  {
+    "revision": "d21ed065f47d1cef6bc8e1428844b128",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
